1624e2b1ca71bd410b7187f536caed4cd773d425
Ross Fenning <ross.fenning@gmail.com>
2014-04-02 12:55:41 +0100

    Added support for finding all RDFa attributes in the page. then used this to display images.

--- indexer/__init__.py
+++ indexer/__init__.py
@@ -1,11 +1,22 @@
 from elasticsearch import Elasticsearch
 import feedparser
-
+from rdflib import Graph, URIRef
 
 def index_item(item, uri):
+
+    if uri.startswith('http://'):
+        for p, o in read_page_for_metadata(uri):
+            item[p] = o
+
     es = Elasticsearch()
     print u'Pushing: {0}'.format(item['http://schema.org/headline'])
     es.index(index='search', id=uri, doc_type='item', body=item)
+
+
+def read_page_for_metadata(url):
+    g = Graph()
+    g.parse(url, format="rdfa1.1")
+    return g.predicate_objects(URIRef(url))
 
 
 def convert_entry_to_item(entry):
@@ -14,8 +25,8 @@ def convert_entry_to_item(entry):
         'http://schema.org/url': entry.link,
         'http://schema.org/description': entry.summary
     }
-    # Use the URL as the key
-    uri = entry.link
+
+    uri = unicode(entry.link).rsplit('#')[0].strip()
     return uri, item
 
 
@@ -23,6 +34,7 @@ def index_feed(feed_url):
     news = feedparser.parse(feed_url)
     for entry in news.entries:
         item, uri = convert_entry_to_item(entry)
+        uri = unicode(entry.link).rsplit('#')[0].strip()
         index_item(uri, item)
 
 
--- search.py
+++ search.py
@@ -21,13 +21,18 @@ def search():
             'total': results['hits']['total'],
             'hits': [
                 {
-                    'title': hit['_source']['http://schema.org/headline'],
-                    'synopsis': hit['_source']['http://schema.org/description'],
-                    'url': hit['_source']['http://schema.org/url']
+                    'title': try_props(hit['_source'], ['http://ogp.me/ns#title', 'http://schema.org/headline']),
+                    'synopsis': try_props(hit['_source'], ['http://schema.org/description']),
+                    'url': try_props(hit['_source'], ['http://schema.org/url']),
+                    'image': try_props(hit['_source'], ['http://ogp.me/ns#image']),
                 } for hit in results['hits']['hits']
             ]
         }
     }
 
+def try_props(data, prop_list):
+    for prop in prop_list:
+        if prop in data:
+            return data[prop]
 
 run(host='localhost', port=8080, debug=True, reloader=True)
