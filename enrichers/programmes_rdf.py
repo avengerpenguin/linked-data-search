1a90ef8d38da55f791ec5c6e9f9f931fcee45f70
Ross Fenning <ross.fenning@gmail.com>
2015-01-12 23:12:37 +0000

    Reduced the number of URIs the programmes RDF enricher has to loop through.

--- enrichers/programmes_rdf.py
+++ enrichers/programmes_rdf.py
@@ -3,10 +3,7 @@ import re
 
 
 def enrich(graph):
-    for uri in set(list(graph.subjects()) + list(graph.objects())):
+    for uri in graph.subjects(predicate=RDF.type, object=URIRef('http://www.bbc.co.uk/search/schema/ContentItem')):
         if re.match('^http://www.bbc.co.uk/programmes/[a-z0-9]+$', uri):
             graph.parse(str(uri) + '.rdf')
             graph.add((uri, URIRef('http://xmlns.com/foaf/0.1/primaryTopic'), uri + '#programme'))
-
-
-
